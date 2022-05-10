b651e90f4171e869ab5c57fc7bd045eabedc538f
Chakib  Amrani <chakibamrani8@gmail.com>
2022-05-10 23:21:00 +0100

    8 - Rendering Lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import bootstrap from "bootstrap";
+
 class Counter extends React.Component {
-    state = {  } 
+    state = { 
+        count : 0,
+        tags:["tag1","tag2","tag3"]
+         } 
     render() { 
         return (
-            <div>
-                <h1>1</h1>
+            <React.Fragment>
+            <div className="p-4 m-4 d-flex justify-content-between border w-50">
+                <span className="text-danger h3">{this.formatCoutner()}</span>
                 <button className="btn btn-primary">Increment</button>
             </div>
+            <ul>
+            {
+            this.state.tags.map(tag=> 
+            <li key={tag}>
+                {tag}
+            </li>
+                )}
+            </ul>
+          </React.Fragment>
         );
+    }
+
+    formatCoutner(){
+        return this.state.count == 0 ? "Zero" : this.state.count;
     }
 }
  
+
 export default Counter;
