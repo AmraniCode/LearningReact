b9b001b2d234d89c008722c257a4e8f72395179a
Chakib  Amrani <chakibamrani8@gmail.com>
2022-05-10 22:53:12 +0100

    starting counter project

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import bootstrap from "bootstrap";
+class Counter extends React.Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <h1>1</h1>
+                <button className="btn btn-primary">Increment</button>
+            </div>
+        );
+    }
+}
+ 
+export default Counter;
